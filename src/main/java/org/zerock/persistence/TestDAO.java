f3df325660788b25eb3bd290e70901eb569daca8
해린 <@haerin>
2016-11-15 14:24:30 +0900

    add interface

--- src/main/java/org/zerock/persistence/TestDAO.java
+++ src/main/java/org/zerock/persistence/TestDAO.java
@@ -1,5 +1,15 @@
 package org.zerock.persistence;
 
+import org.zerock.domain.TestVO;
+
 public interface TestDAO {
+
+	public void create(TestVO vo) throws Exception;
+
+	public TestVO read(Integer tno) throws Exception;
+
+	public void update(TestVO vo) throws Exception;
+
+	public void delete(Integer tno) throws Exception;
 
 }
